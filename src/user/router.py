705fa38736558d0d22d0cf7e07dc718c95cba7bc
LedxDeliveryFlopp <korstim18@gmail.com>
2024-03-13 21:09:44 +0700

    update user router.py for new structure

--- src/user/router.py
+++ src/user/router.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer
-from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
-from src.settings.depends import get_session
+from src.user.depends import get_user_service
 from src.user.schemas import UserBaseSchemas, UserUpdateSchemas
-from src.user.service import get_current_user, update_user, delete_user
+from src.user.service import UserService
 
 user_router = APIRouter(
     prefix="/user",
@@ -15,26 +14,25 @@ bearer_token = HTTPBearer()
 
 
 @user_router.get("/current-user/", response_model=UserBaseSchemas)
-async def get_current_user_router(request: Request, token: str = Depends(bearer_token), session:
-                                  AsyncSession = Depends(get_session)):
+async def get_current_user_router(request: Request, user: UserService = Depends(
+                                  get_user_service), token: str = Depends(bearer_token)):
     """Роутер получения текущего пользователя"""
-    user = await get_current_user(session=session, request=request)
-    return user
+    current_user = await user.get_current_user()
+    return current_user
 
 
 @user_router.patch("/update-user/", response_model=UserBaseSchemas)
-async def update_user_router(user_schemas_update: UserUpdateSchemas, request: Request,
-                             token: str = Depends(bearer_token),  session:
-                             AsyncSession = Depends(get_session)):
+async def update_user_router(schemas: UserUpdateSchemas, request: Request,
+                             user: UserService = Depends(get_user_service),
+                             token: str = Depends(bearer_token)):
     """Роутер обновления пользователя"""
-    user = await update_user(session=session, request=request,
-                             user_schemas_update=user_schemas_update)
-    return user
+    updated_user = await user.update_user(user_schemas_update=schemas)
+    return updated_user
 
 
 @user_router.delete("/delete-user/")
-async def delete_user_router(request: Request, token: str = Depends(bearer_token), session:
-                                  AsyncSession = Depends(get_session)):
+async def delete_user_router(request: Request, user: UserService = Depends(get_user_service),
+                             token: str = Depends(bearer_token)):
     """Роутер удаление пользователя"""
-    user = await delete_user(session=session, request=request)
+    deleted_user = await user.delete_user()
     return user
